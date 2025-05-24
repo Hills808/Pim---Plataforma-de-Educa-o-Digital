831946914543eb84383dcb3a820b6c30e0f62996
Henrique Alexandre <117482275+Hills808@users.noreply.github.com>
2025-05-24 14:51:43 -0300

    Delete DEV_Junior directory

--- DEV_Junior/main.py
+++
@@ -1,9 +0,0 @@
-soma = 1 + 1
-multiplicacao = 4 * 4
-divisao = 30 / 3
-potencia = 7 ** 2
-
-print("soma", soma)
-print("multiplicaca", multiplicacao)
-print("divisao", divisao)
-print("pontecia", potencia)
